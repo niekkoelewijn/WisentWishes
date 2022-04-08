6fd9ee159e46399b9287794bdc836da848dd4520
niekkoelewijn <75852458+niekkoelewijn@users.noreply.github.com>
2022-04-08 08:57:19 +0200

    now correct study areas per date

--- R/StudyArea.R
+++ R/StudyArea.R
@@ -21,37 +21,23 @@ library(raster)
 library(dplyr)
 
 # Create vector of paths to study area shapefiles
-VeluwePath <- "~/WisentWishes/MScThesisData/GISFilesAreas/Veluwe/Wisentraster.shp"
-MaashorstPath <- "~/WisentWishes/MScThesisData/GISFilesAreas/Maashorst/RE__GIS_data_Maashorst/StroomrasterBegrazingsgebied.shp"
+VeluwePath <- "~/WisentWishes/MScThesisData/GISFilesAreas/Veluwe/WisentGebiedVeluwe.shp"
+Maashorst2016Path <- "~/WisentWishes/MScThesisData/GISFilesAreas/Maashorst/WisentGebied2016.shp"
+Maashorst20172021Path <- "~/WisentWishes/MScThesisData/GISFilesAreas/Maashorst/WisentGebied2017-2021.shp"
+Maashorst2022Path <- "~/WisentWishes/MScThesisData/GISFilesAreas/Maashorst/WisentGebied2022.shp"
 SlikkenvdHeenPath <- "~/WisentWishes/MScThesisData/GISFilesAreas/SlikkenvdHeen/WisentGebied.shp"
-KraansvlakPath <- "~/WisentWishes/MScThesisData/GISFilesAreas/Kraansvlak/Wisentgebied2014/Wisentgebied2014.shp"
-PathVec <- c(VeluwePath, MaashorstPath, SlikkenvdHeenPath, KraansvlakPath)
+KraansvlakPath <- "~/WisentWishes/MScThesisData/GISFilesAreas/Kraansvlak/WisentGebiedKraansvlak.shp"
+PathVec <- c(VeluwePath, Maashorst2016Path,Maashorst20172021Path,Maashorst2022Path, SlikkenvdHeenPath, KraansvlakPath)
 
-# Read tudy area shapefiles into R
+# Read study area shapefiles into R
 VeluweStudyArea <- readOGR(PathVec[1])
-MaashorstStudyArea <- readOGR(PathVec[2])
-SlikkenvdHeenStudyArea <- readOGR(PathVec[3])
-KraansvlakStudyArea <- readOGR(PathVec[4])
+Maashorst2016StudyArea <- readOGR(PathVec[2])
+Maashorst20172021StudyArea <- readOGR(PathVec[3])
+Maashorst2022StudyArea <- readOGR(PathVec[4])
+SlikkenvdHeenStudyArea <- readOGR(PathVec[5])
+KraansvlakStudyArea <- readOGR(PathVec[6])
 
-# Transform sp to sf
-VeluweStudyAreaSf <- st_as_sf(VeluweStudyArea) # RD new
-MaashorstStudyAreaSf <- st_as_sf(MaashorstStudyArea) # WGS 84 / Pseudo-Mercator
-SlikkenvdHeenStudyAreaSf <- st_as_sf(SlikkenvdHeenStudyArea) # WGS 84
-KraansvlakStudyAreaSf <- st_as_sf(KraansvlakStudyArea) # RD new
 
-# Transform proj4string to WGS84
-TargetCRS <- st_crs(SlikkenvdHeenStudyAreaSf)
 
-st_transform(x = VeluweStudyAreaSf, crs = TargetCRS)
-st_transform(x = MaashorstStudyAreaSf, crs = TargetCRS)
-st_transform(x = KraansvlakStudyAreaSf, crs = TargetCRS)
 
-# Plot study areas in map of the Netherlands
-path <- "~/WisentWishes/MScThesisData/GISFilesAreas/RDownloads/world-administrative-boundaries/world-administrative-boundaries.shp"
-World <- readOGR(path)
-WorldSf <- st_as_sf(World)
-Netherlands <- WorldSf[WorldSf$iso3 == "NLD",]
 
-for(i in 1:length(PathVec)){
-  
-}

f2cfe7cff4123e240042a0351406a684ab90d86d
niekkoelewijn <75852458+niekkoelewijn@users.noreply.github.com>
2022-05-16 10:14:00 +0200

    changed file structure

---
+++ R/EnvironmentalVariables/LandusePreprocessing.R
@@ -0,0 +1,9 @@
+# Course: REG-80436 / MSc Thesis
+# Project: Wisent Wishes
+# Name: Niek Koelewijn
+# Organisation: Wageningen University
+# Supervisor: dr. ir. H.J. de Knegt
+# Year: 2022
+
+### Script to adapt LGN2021 End product of this script is ready to assign
+### environmental variables to.
